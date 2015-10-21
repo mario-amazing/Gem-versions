b6f1ec7371acac8d121dcaa7b3da631d980fa38c
megosparta <megosparta@mail.ru>
2015-10-21 23:43:14 +0300

    Rewrite args

--- lib/gemfiler/args.rb
+++ lib/gemfiler/args.rb
@@ -1,29 +1,28 @@
 # parse @argv
 module Gemfiler
   class Args
+    SIGN_FORMAT = /(~>|>=|<)/
+    VER_FORMAT = /(\d+(?:\.[0-9A-Za-z-]+)*)/
+
     def initialize(argv)
       @argv = argv
     end
 
-    def gem_name
-      if @argv.first.nil? || !(@argv.first =~ /[^-A-z]/).nil?
-        puts 'You missed the name of gem or used invalid characters.Try again.'
-        puts 'Usage: gemver.rb [gem name] [gem version]*'
-        puts '* - optional params, in a format compatible with Gemfile'
-        exit
-      else
-        @argv.first.downcase
+    def parse
+      gem_name = @argv.shift.downcase
+      conditions = []
+      @argv.each do |condition|
+        sign, ver = condition.squeeze(' ').split(' ')
+        validate_condition!(sign, ver)
+        conditions << { sign: sign, ver: ver }
       end
+      { gem_name: gem_name, conditions: conditions }
     end
 
-    def parse
-      params =
-        ("#{@argv[1]}".scan(/(~>|>=|<)\s+(\d+(?:\.[0-9A-Za-z-]+)*)/)).flatten
-      params +=
-        ("#{@argv[2]}".scan(/(<)\s+(\d+(?:\.[0-9A-Za-z-]+)*)/)).flatten
-      params[1] = Gem::Version.new(params[1]) if !params[1].nil?
-      params[3] = Gem::Version.new(params[3]) if !params[3].nil?
-      params
+    def validate_condition!(sign, ver)
+      if sign.nil? || ver.nil? || sign !~ SIGN_FORMAT || ver !~ VER_FORMAT
+        raise 'Wrong conditions format'
+      end
     end
   end
 end
--- test/vr.rb
+++ test/vr.rb
@@ -1,8 +1,4 @@
 #!/usr/bin/env ruby
 require_relative '../lib/gemfiler'
 
-args = Gemfiler::Args.new(ARGV)
-ver = Gemfiler::Versions.new
-disp = Gemfiler::Display.new
-versions = ver.display_versions(args.gem_name)
-disp.display_versions(versions, args.parse)
+ver = Gemfiler::Versions.new(Gemfiler::Args.new(ARGV).parse).to_s
